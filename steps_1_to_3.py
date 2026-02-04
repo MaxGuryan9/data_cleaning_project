ef372c1ffc13e2ac0aa4ac10f26a36b3b53c4388
Max Guryan <rhk2dx@virginia.edu>
2026-02-04 06:48:21 +0000

    initial edits to step 2

--- steps_1_to_3.py
+++ steps_1_to_3.py
@@ -1,9 +1,20 @@
 #%% [markdown]
 # # Step 1: Identify a question that the dataset can answer:
-# **College Completion Dataset:**
-# Is a student's pre-college academic performace (SAT) a good determinant of their likelihood of completing college? \
+# **College Completion Dataset:** \
+# Based on the amount of aid a school provides to students, both merit and financial, whether the students at that school are more likely to complete college?
 
-# **Job Placement Dataset:**
-# Is a student's pre-college academic performance (Standardized Test Scores) a good determinant of the salary they will receive after graduation?
+# **Job Placement Dataset:** \
+# Is there a correlation between the performance on standardized tests and degree performance percentile and whether you have gotten a job or not? 
+
+# %% [markdown]
+# # Step 2: Independent Business Metric and Data Cleaning:
+# **College Completion Dataset:** 
+# - **Independent Business Metric:** \
+# `awards_per_value_region` = The number of awards per 100 full-time undergraduates compared to the region average. \ 
+
+# **Job Placement Dataset:** 
+# - **Independent Business Metric:** \
+# `status` = Whether or not the candidate is employed or not (1 = employed, 0 = unemployed).
+# %%
 
 # %%

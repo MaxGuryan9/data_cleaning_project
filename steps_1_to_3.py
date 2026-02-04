1ea1e140738768a2ef4bba78ba65c83fbb046a07
Max Guryan <rhk2dx@virginia.edu>
2026-02-04 05:48:23 +0000

    completed step 1

---
+++ steps_1_to_3.py
@@ -0,0 +1,9 @@
+#%% [markdown]
+# # Step 1: Identify a question that the dataset can answer:
+# **College Completion Dataset:**
+# Is a student's pre-college academic performace (SAT) a good determinant of their likelihood of completing college? \
+
+# **Job Placement Dataset:**
+# Is a student's pre-college academic performance (Standardized Test Scores) a good determinant of the salary they will receive after graduation?
+
+# %%
